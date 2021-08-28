53b0e95a85380a2e73a031b822a2b07d1aa1f640
kunkun kurniawan <kunkun.kurniawan@gokampus.com>
2021-08-28 08:11:09 +0700

    init service collection helper and add default datetime format

---
+++ Helper/ServiceCollectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Pdam.Common.Shared.Helper
+{
+    public static class ServiceCollectionHelper
+    {
+        public static IMvcBuilder AddDateTimeFormat(this IMvcBuilder mvcBuilder)
+        {
+            mvcBuilder.AddNewtonsoftJson(o =>
+            {
+                o.SerializerSettings.DateFormatString = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";
+                o.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+            return mvcBuilder;
+        }
+    }
+}
