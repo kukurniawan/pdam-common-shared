8a2bbbd741f2641a6ee4a1f1119fe7eb4736a233
kunkun kurnia wirawan <kurnia.wirawan@gmail.com>
2023-05-21 16:41:39 +0700

    update refresh token request

--- Security/RefreshTokenRequest.cs
+++ Security/RefreshTokenRequest.cs
@@ -1,8 +1,22 @@
-﻿namespace Pdam.Common.Shared.Security;
+﻿using MediatR;
 
-public class RefreshTokenRequest
+namespace Pdam.Common.Shared.Security;
+
+/// <summary>
+/// 
+/// </summary>
+public class RefreshTokenRequest : IRequest<LoginResponse>
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public string RefreshToken { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
     public Guid Uid { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string CompanyId { get; set; }
 }
